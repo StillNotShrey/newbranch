13c52cc1b7f949901eb9c3c18df99c5cc64a201c
StillNotShrey <2105408@kiit.ac.in>
2025-06-13 14:51:43 +0530

    commited

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+
+def excpt():
+    employee = {
+        "id" : 300,
+        "Name" : "shrey",
+        "desp" : "somebody removed",
+        'salary' : 9999
+    }
+    
+    try:
+        print("current emp record: ",employee)
+        
